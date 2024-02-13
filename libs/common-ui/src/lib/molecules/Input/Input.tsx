cae7b9a485a9a2e4ee232ecdec4ddeb992d57ac4
lukczar1706 <lcse.2001@gmail.com>
2024-02-13 13:56:18 +0100

    feat: Viewport

--- libs/common-ui/src/lib/molecules/Input/Input.tsx
+++ libs/common-ui/src/lib/molecules/Input/Input.tsx
@@ -1,43 +1,41 @@
-import type { ComponentProps } from 'react';
+import { forwardRef, type ComponentProps, type Ref } from 'react';
 import clsx from 'clsx';
 import { v4 as uuidv4 } from 'uuid';
 import { Label } from '../../atoms/Label';
 
-type Props = {
+type Props = ComponentProps<'input'> & {
   label: string;
 };
 
-export const Input = ({
-  type,
-  label,
-  placeholder,
-  className,
-  value,
-  onChange,
-  ...rest
-}: ComponentProps<'input'> & Props) => {
-  const id = uuidv4();
-  const classes = clsx(
-    'block',
-    'mb-4',
-    'border',
-    'border-gray-300',
-    'rounded-md',
-    className
-  );
+export const Input = forwardRef(
+  (
+    { type, label, placeholder, className, value, onChange, ...rest }: Props,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const id = uuidv4();
+    const classes = clsx(
+      'block',
+      'mb-4',
+      'border',
+      'border-gray-300',
+      'rounded-md',
+      className
+    );
 
-  return (
-    <div>
-      <Label id={id}>{label}</Label>
-      <input
-        id={id}
-        type={type}
-        placeholder={placeholder}
-        className={classes}
-        defaultValue={value}
-        onChange={onChange}
-        {...rest}
-      />
-    </div>
-  );
-};
+    return (
+      <div>
+        <Label id={id}>{label}</Label>
+        <input
+          id={id}
+          type={type}
+          ref={ref}
+          placeholder={placeholder}
+          className={classes}
+          defaultValue={value}
+          onChange={onChange}
+          {...rest}
+        />
+      </div>
+    );
+  }
+);
