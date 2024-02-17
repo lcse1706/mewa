1a225b38e70a22b3fd37c7c4cf3b57ffee61fab5
lukczar1706 <lcse.2001@gmail.com>
2024-02-17 14:04:48 +0100

    Protected Routes

---
+++ apps/frontend/src/app/pages/RegistrationPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@ems/common-ui';
+import { RegistrationForm } from '../components/RegisterationForm';
+
+export const RegistrationPage = () => {
+  return (
+    <div>
+      <Header>Registration</Header>
+      <RegistrationForm />
+    </div>
+  );
+};
