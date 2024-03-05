fb91812fc24642b4b9e9a21e206f8c3db686d2cf
lukczar1706 <lcse.2001@gmail.com>
2024-03-05 21:44:56 +0100

    React Context Hook Pattern

--- apps/frontend/src/app/pages/HomePage.tsx
+++ apps/frontend/src/app/pages/HomePage.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@ems/common-ui';
+import { AuthInfo } from '../components/Auth/AuthInfo';
+import { AuthContextProvider } from '../components/Auth/AuthContext';
 
 export const HomePage = () => {
   return (
     <div>
       <Header>Home</Header>
       <p>Hello from routed page</p>
+      <AuthContextProvider>
+        <AuthInfo />
+      </AuthContextProvider>
     </div>
   );
 };
