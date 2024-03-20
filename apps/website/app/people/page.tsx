e445146b66e8da5c07cf7ad715a8736da2b2cf63
lukczar1706 <lcse.2001@gmail.com>
2024-03-20 20:04:03 +0100

    feat: app structure

---
+++ apps/website/app/people/page.tsx
@@ -0,0 +1,8 @@
+export default function PeoplePage() {
+  return (
+    <div>
+      <h1>People</h1>
+      <p>Lorem Ipsum ...</p>
+    </div>
+  );
+}
---
+++ apps/website/app/reviews/page.tsx
@@ -0,0 +1,8 @@
+export default function ReviewsPage() {
+  return (
+    <div>
+      <h1>Reviews</h1>
+      <p>Lorem Ipsum ...</p>
+    </div>
+  );
+}
