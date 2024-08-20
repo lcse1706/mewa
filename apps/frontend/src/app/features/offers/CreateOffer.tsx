531de1c12cd45c0de66234773c4a827b878989d1
lukczar1706 <lcse.2001@gmail.com>
2024-08-20 13:51:43 +0200

    feat: CreateOffer component

--- apps/frontend/src/app/features/offers/CreateOffer.tsx
+++ apps/frontend/src/app/features/offers/CreateOffer.tsx
@@ -1,3 +1,42 @@
+import { Button, Input } from '@ems/common-ui';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { createOfferSchema, CreateReviewDto } from './types';
+import { zodResolver } from '@hookform/resolvers/zod';
 export const CreateOffer = () => {
-  return <div>Create Offer</div>;
+  // const [isError, setIsError] = useState(false);
+  // const { push, refresh } = useRouter();
+  // const [isPending, startTransition] = useTransition();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm<CreateReviewDto>({
+    resolver: zodResolver(createOfferSchema),
+  });
+
+  const submitHandler: SubmitHandler<CreateReviewDto> = async (data) => {
+    console.log({ data });
+  };
+
+  return (
+    <div>
+      <h3>Create Offer</h3>
+      {isSubmitting && <p>Oh no ! Server error !</p>}
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <Input {...register('role')} label="Role" error={errors.role} />
+        <Input
+          {...register('description')}
+          label="Description"
+          error={errors.description}
+        />
+        <Input
+          {...register('salary', { valueAsNumber: true })}
+          label="Salary"
+          type="number"
+          error={errors.salary}
+        />
+        <Button label="Submit" type="submit" />
+      </form>
+    </div>
+  );
 };
