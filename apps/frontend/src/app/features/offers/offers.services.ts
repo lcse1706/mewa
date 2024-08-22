0eaaa3322cc29cd450176ac7004231708d859742
lukczar1706 <lcse.2001@gmail.com>
2024-08-22 14:29:48 +0200

    feat: offer service added

--- apps/frontend/src/app/features/offers/offers.services.ts
+++ apps/frontend/src/app/features/offers/offers.services.ts
@@ -2,5 +2,5 @@ import { CreateOfferDto } from './types';
 import axios from 'axios';
 
 export const createOffer = (data: CreateOfferDto) => {
-  return axios.post('http://localhost:3002/api/offers');
+  return axios.post('http://localhost:3000/api/offers');
 };
