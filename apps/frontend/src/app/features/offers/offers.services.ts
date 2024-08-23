a8a4a1eecff115ffabc0d79fce52fecfa43066f9
lukczar1706 <lcse.2001@gmail.com>
2024-08-23 12:56:54 +0200

    feat: Authorization

--- apps/frontend/src/app/features/offers/offers.services.ts
+++ apps/frontend/src/app/features/offers/offers.services.ts
@@ -1,6 +1,13 @@
 import { CreateOfferDto } from './types';
 import axios from 'axios';
 
+//TODO save in env variables
+const API_KEY = '1234';
+
 export const createOffer = (data: CreateOfferDto) => {
-  return axios.post('http://localhost:3000/api/offers');
+  return axios.post('http://localhost:3000/api/offers', data, {
+    headers: {
+      Authorization: API_KEY,
+    },
+  });
 };
