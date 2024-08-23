f6b20b911c2d8ab63c67344a998eb5247961fe9e
lukczar1706 <lcse.2001@gmail.com>
2024-08-23 13:43:31 +0200

    api key moved to local env

--- apps/frontend/src/app/features/offers/offers.services.ts
+++ apps/frontend/src/app/features/offers/offers.services.ts
@@ -2,7 +2,7 @@ import { CreateOfferDto } from './types';
 import axios from 'axios';
 
 //TODO save in env variables
-const API_KEY = '1234';
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const createOffer = (data: CreateOfferDto) => {
   return axios.post('http://localhost:3000/api/offers', data, {
