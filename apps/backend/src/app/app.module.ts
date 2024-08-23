6e0b778452269c547b77dca913064ecde89f8df3
lukczar1706 <lcse.2001@gmail.com>
2024-08-23 12:56:16 +0200

    feat: OffersModule

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewsModule } from './reviews/reviews.module';
+import { OffersModule } from './offers/offers.module';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ReviewsModule],
+  imports: [ReviewsModule, OffersModule],
   controllers: [AppController],
   providers: [AppService],
 })
