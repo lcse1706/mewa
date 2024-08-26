70038b77fcea47fe2792ceb21a49fec255c59fab
lukczar1706 <lcse.2001@gmail.com>
2024-08-26 14:25:40 +0200

    feat: fetch offers

--- apps/frontend/src/app/features/offers/offers.services.ts
+++ apps/frontend/src/app/features/offers/offers.services.ts
@@ -1,6 +1,14 @@
+import { type AxiosResponse } from 'axios';
+
 import { CreateOfferDto } from './types';
+
 import { api } from '../../config/api';
+import { OffersListDto } from '@ems/contracts';
 
 export const createOffer = (data: CreateOfferDto) => {
   return api.post('offers', data);
+};
+
+export const fetchOffers = (): Promise<AxiosResponse<OffersListDto[]>> => {
+  return api.get('offers');
 };
