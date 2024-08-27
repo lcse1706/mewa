bcebd9661c689d5ea38657549fd297f99973a44e
lukczar1706 <lcse.2001@gmail.com>
2024-08-27 16:04:23 +0200

    feat: OpenAi assistant

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewsModule } from './reviews/reviews.module';
 import { OffersModule } from './offers/offers.module';
+import { AIModule } from './ai/ai.module';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ReviewsModule, OffersModule],
+  imports: [ReviewsModule, OffersModule, AIModule],
   controllers: [AppController],
   providers: [AppService],
 })
