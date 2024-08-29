09a3faf7a4cbd80dd4013541c264441808d55c04
lukczar1706 <lcse.2001@gmail.com>
2024-08-29 10:30:48 +0200

    feat: AI assistant

--- apps/backend/src/app/ai/ai.service.ts
+++ apps/backend/src/app/ai/ai.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
 
 import { CompletionsDto } from './dtos/completions.dto';
+import { AssistantDto } from './dtos/assistant.dto';
+import { threadId } from 'worker_threads';
 
 @Injectable()
 export class AIService {
@@ -26,6 +28,56 @@ export class AIService {
         userPrompt: completions.message,
         response: completion,
       },
+    };
+  }
+
+  async askAssistant(assistantDto: AssistantDto) {
+    const assistant = await this.openai.beta.assistants.retrieve(
+      process.env.OPENAI_ASSISTANT_ID
+    );
+    const assistantID = assistant.id;
+
+    const thread = await this.openai.beta.threads.create();
+    const threadId = thread.id;
+
+    const messages = await this.openai.beta.threads.messages.create(threadId, {
+      role: 'user',
+      content: assistantDto.message,
+    });
+
+    let run = await this.openai.beta.threads.runs.create(threadId, {
+      assistant_id: assistantID,
+    });
+
+    const runId = run.id;
+
+    run = await this.openai.beta.threads.runs.retrieve(threadId, runId);
+    //queued, in_progress, cancelling
+
+    while (['queued', 'in_progress', 'cancelling'].includes(run.status)) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      run = await this.openai.beta.threads.runs.retrieve(threadId, runId);
+
+      if (run.status === 'completed') {
+        const outputMessage = await this.openai.beta.threads.messages.list(
+          threadId
+        );
+        return {
+          assistant,
+          thread,
+          messages,
+          run,
+          outputMessage,
+        };
+      }
+    }
+
+    return {
+      assistant,
+      thread,
+      messages,
+      run,
     };
   }
 }
